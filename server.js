26fb98cd19de2b374ba2ba08bd4d894fb05dda85
Dami-Karv <damikarv@gmail.com>
2024-06-30 15:17:51 +0300

    kleidia pano sto server lol

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const { getLyrics, getSong, searchSong, getSongById } = require('genius-lyrics-api');
-require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
+const GENIUS_ACCESS_TOKEN = 'bvo-JN81MSCiz4axQKZVhcBPFTwNiYXrmqXAr2A0_Et_wbtfPe4kU4h2wYO5hQ7o';
+
 app.get('/search', async (req, res) => {
   const query = req.query.q;
   const options = {
-    apiKey: process.env.REACT_APP_GENIUS_ACCESS_TOKEN,
+    apiKey: GENIUS_ACCESS_TOKEN,
     title: query,
     artist: '',
     optimizeQuery: true
@@ -30,7 +31,7 @@ app.get('/songs/:id', async (req, res) => {
   const songId = req.params.id;
 
   try {
-    const song = await getSongById(songId, process.env.REACT_APP_GENIUS_ACCESS_TOKEN);
+    const song = await getSongById(songId, GENIUS_ACCESS_TOKEN);
     res.json(song);
   } catch (error) {
     console.error('Error fetching song details:', error.message);
@@ -40,11 +41,6 @@ app.get('/songs/:id', async (req, res) => {
 
 app.get('/lyrics', async (req, res) => {
   const songPath = req.query.path;
-  const options = {
-    apiKey: process.env.REACT_APP_GENIUS_ACCESS_TOKEN,
-    title: '',
-    artist: ''
-  };
 
   try {
     const lyrics = await getLyrics(songPath);
